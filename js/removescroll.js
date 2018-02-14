f6d1d14b7305c65cf3defeafe864724595f52662
Pieter Mantel <4pietermantel@gmail.com>
2018-02-14 09:50:03 +0100

    Size windows

---
+++ js/removescroll.js
@@ -0,0 +1,20 @@
+function scrollBars(){
+	var body= $('body')[0];
+	return {
+		vertical:body.scrollHeight>body.clientHeight,
+		horizontal:body.scrollWidth>body.clientWidth
+	}
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+$(document).ready(function(){
+	setInterval(1000,sizeWindows());
+});
+
+function sizeWindows() {
+	$(".window").css("height",
+	$(window).height().toString().concat("px"));
+}
